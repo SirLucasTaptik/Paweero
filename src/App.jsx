562e19ecf576aea3f2ebde03f42c287b0c5460d7
SirLucas <101318158+SirLucasTaptik@users.noreply.github.com>
2026-05-11 20:11:07 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 // ─── SUPABASE ─────────────────────────────────────────────────────────────────
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+console.log("SUPABASE URL:", import.meta.env.VITE_SUPABASE_URL);
 const db = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // ─────────────────────────────────────────────────────────────────────────────
